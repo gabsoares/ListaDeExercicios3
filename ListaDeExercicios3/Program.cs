42dae7d2c7a14be3f7d17549b3cb1250b14e518d
gabsoares <soaresgabriel68@gmail.com>
2023-05-17 00:02:44 -0300

    Add project files.

---
+++ ListaDeExercicios3/Program.cs
@@ -0,0 +1,26 @@
+﻿string option = "";
+
+while (option != "N" && option != "n")
+{
+    Console.WriteLine("Digite a temperatura em Celsius para converter em Fahrenheit");
+    double temperature = Convert.ToDouble(Console.ReadLine());
+
+    double fahrenheit = (temperature * 1.8) + 32;
+
+    Console.WriteLine($"A temperatura convertida eh {fahrenheit}F");
+
+
+    Console.Write("Digite S/N para continuar ou não: ");
+    option = Console.ReadLine();
+}
+
+
+
+
+
+
+
+
+
+
+
